df5e6b9440ac61041f42d4fe5776a16f12090ca1
lilithlarose <lilith.larose@thoughtworks.com>
2020-07-01 13:26:39 +1000

    first implementation of landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import rose from './centre_rose.svg';
+import centre_text from './centre_rose_text.svg'
 import './App.css';
+import 'socicon/css/socicon.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={rose} className="Rose rotates" alt="logo" />
+          <img src={centre_text} className="Rose rotates" alt="logo" />
       </header>
+        <footer className="App-footer gradient">
+            <div>Software Developer -
+            Melbourne, Australia</div>
+            <div className="footer-right">
+                {/*eslint-disable-next-line*/}
+                <a href="https://twitter.com/Maveritan" className="socicon-twitter social"/>
+                {/*eslint-disable-next-line*/}
+                <a href="https://github.com/Maveritan" className="socicon-github social"/>
+            </div>
+        </footer>
     </div>
   );
 }
+
 
 export default App;
